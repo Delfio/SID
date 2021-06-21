c3ec4728a993ff55813081509d1b2897f42ade5c
Delfio Francisco <delfio_eu@hotmail.com>
2021-06-20 23:13:53 -0400

    adicionando função de toast

---
+++ components/Toast/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'react-toastify/dist/ReactToastify.css';
+  
+import { ToastContainer, toast } from 'react-toastify';
+
+export default function Toast({message, go}) {
+    
+    return (
+        <ToastContainer />
+    )
+}
