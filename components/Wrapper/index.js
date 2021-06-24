21b060be9c2666f3f634f682980ccafd783684ff
Delfio Francisco <delfio_eu@hotmail.com>
2021-06-24 19:39:04 -0400

    adicionando a logo principal e corrigindo problemas de estilização. fix #4

--- components/Wrapper/index.js
+++ components/Wrapper/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import WrapperStyle from "./wrapper.module.css";
+import Image from 'next/image';
 
 export default function Wrapper({ children, title }) {
   return (
     <div className={WrapperStyle.main}>
       <section className={WrapperStyle.titleWrapper}>
-        <h1>{title}</h1>
+        <Image 
+          src="/img/logo.png"
+          alt={title}
+          // className="rounded mx-auto d-block"
+          width="100%"
+          height="auto"
+        />
       </section>
       <section className={WrapperStyle.mainContent}>{children}</section>
     </div>
